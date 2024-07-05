9313a97b7f983fefeeb035712d39e32706492fdc
sai karthik <saifedev@gmail.com>
2024-07-05 10:11:40 +0000

    initail changes

--- src/main/java/com/example/hrChatbot/HrChatbotApplication.java
+++ src/main/java/com/example/hrChatbot/HrChatbotApplication.java
@@ -2,6 +2,8 @@ package com.example.hrChatbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class HrChatbotApplication {
@@ -9,5 +11,15 @@ public class HrChatbotApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HrChatbotApplication.class, args);
 	}
+
+	@RestController
+	class HelloController{
+		@GetMapping("/")
+		public String hello() {
+            return "Hello, World!";
+        }
+		
+	} 
+	
 
 }
