eeb5d6beb51168e9279df2ccd70188234f01d28c
saife237 <saifedev@gmail.com>
2024-07-05 09:53:15 +0000

    first commit

---
+++ src/main/java/com/example/hrChatbot/HrChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.example.hrChatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrChatbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hrChatbot/HrChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hrChatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
